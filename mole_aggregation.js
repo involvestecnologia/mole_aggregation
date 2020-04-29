c1ab0fb9950e05276f88599d1ae516acb63f06cf
Renan Berto <renanbertoo@gmail.com>
2020-04-29 17:59:56 -0300

    Include pagination of search/insert

--- mole_aggregation.js
+++ mole_aggregation.js
@@ -3,37 +3,53 @@ require('dotenv').config()
 const { Client } = require('@elastic/elasticsearch')
 const moment = require('moment')
 
-const client = new Client({ node: process.env.ELASTICSEARCH_URL})
+const client = new Client({ node: process.env.ELASTICSEARCH_URL })
 
-async function run () {
-  let indiceName = process.env.ELASTICSEARCH_INDEX_INPUT
-  let initialDate = moment().utc().startOf('day').add(-1, 'days').format()
-  let endDate = moment().utc().endOf('day').add( -1, 'days').format()
-  
-  const { body } = await client.sql.query({
+const indiceName = process.env.ELASTICSEARCH_INDEX_INPUT
+const initialDate = moment().utc().startOf('day').add(-1, 'days').format()
+const endDate = moment().utc().endOf('day').add(-1, 'days').format()
+const meta = { index: { _index: process.env.ELASTICSEARCH_INDEX_OUTPUT } }
+
+async function run() {
+
+  let { body } = await client.sql.query({
     body: {
-      query: `SELECT database, collection, operation, COUNT(operation) as count FROM "${indiceName}" WHERE timestamp BETWEEN '${initialDate}' AND '${endDate}' GROUP BY database,collection,operation`
-    }
+      query: `SELECT database, collection, operation, COUNT(operation) as count FROM "${indiceName}" WHERE timestamp BETWEEN '${initialDate}' AND '${endDate}' GROUP BY database,collection,operation`,
+    },
   }).catch((err) => {
     console.log(err.body.error.reason)
   })
 
+  executer(body.cursor, body.rows, body.columns)
+}
+
+
+async function executer(cursorID, rows, columns) {
   let payload = []
-  const meta = { index:{ _index: process.env.ELASTICSEARCH_INDEX_OUTPUT}}
-    
-  body.rows.forEach((row) => {
+
+  rows.forEach((row) => {
     payload.push(meta)
     let data = {}
     for (var i = 0; i < row.length; i++) {
-      data[body.columns[i].name] = row[i]
+      data[columns[i].name] = row[i]
       data.timestamp = initialDate
     }
     payload.push(data)
   })
-  
+
   await client.bulk({ body: payload }).catch((err) => {
     console.log(err.body.error.reason)
   })
+
+  if (cursorID) {
+    let { body } = await client.sql.query({
+      body: { cursor: cursorID },
+    }).catch((err) => {
+      console.log(err.body.error.reason)
+    })
+
+    executer(body.cursor, body.rows, columns)
+  }
 }
 
 run()
